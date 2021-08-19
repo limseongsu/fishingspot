84bcd5aeb4b9d428ddbbade026b039b52fb895f5
imseongsu <tjdtn2323@naver.com>
2021-08-19 10:46:48 +0900

    color 변경 및 버튼 수정

--- lib/view/detail/detail.dart
+++ lib/view/detail/detail.dart
@@ -16,77 +16,70 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffDDFFFF),
-      appBar: NewGradientAppBar(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 1,
+        leading: BackButton(
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.white,
         title: Text(
           '${widget.item.fshlcNm}',
           style: TextStyle(fontSize: 25, color: Colors.black),
         ),
         centerTitle: true,
-        gradient:
-            LinearGradient(colors: [Color(0Xff60B1FF), Color(0XFF60DCFF)]),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(height: 300, child: GoogleMapPage(widget.item)),
-            Container(
-              height: 310,
-              width: 420,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Color(0Xff60B1FF), Color(0XFF60DCFF)],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left:15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 10),
-                    Text(
-                      '거리 : ${widget.item.meter! / 1000}km' ,
-                      style: TextStyle(fontSize: 20, color: Colors.black),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      '가격 : ${widget.item.useCharge}원',
-                      style: TextStyle(fontSize: 20, color: Colors.black),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      '유형 : ${widget.item.fshlcType}',
-                      style: TextStyle(fontSize: 20, color: Colors.black),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      '대상어종 : ${widget.item.kdfsh}',
-                      style: TextStyle(fontSize: 20, color: Colors.black),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      '구조물 : ${widget.item.wtrcFcltyType}',
-                      style: TextStyle(fontSize: 20, color: Colors.black),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      '주소 : ${widget.item.rdnmadr}',
-                      style: TextStyle(fontSize: 20, color: Colors.black),
-                    ),
-
-                    SizedBox(height: 10),
-                    Text(
-                      '전화번호 :${widget.item.phoneNumber}',
-                      style: TextStyle(fontSize: 20, color: Colors.black),
-                    ),
-                  ],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(height: 300, child: GoogleMapPage(widget.item)),
+          Padding(
+            padding: const EdgeInsets.only(left:15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 10),
+                Text(
+                  '거리 : ${widget.item.meter! / 1000}km' ,
+                  style: TextStyle(fontSize: 20, color: Colors.black),
                 ),
-              ),
+                SizedBox(height: 10),
+                Text(widget.item.useCharge != 'null' ?
+                  '가격 : ${widget.item.useCharge}원' : '가격 : 정보 없음',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+                SizedBox(height: 10),
+                Text(widget.item.fshlcType != 'null' ?
+                  '유형 : ${widget.item.fshlcType}' : '유형 : 정보 없음',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+                SizedBox(height: 10),
+                Text(widget.item.kdfsh != 'null' ?
+                  '대상어종 : ${widget.item.kdfsh}' :'대상어종 : 정보 없음',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+                SizedBox(height: 10),
+                Text(widget.item.wtrcFcltyType != 'null' ?
+                  '구조물 : ${widget.item.wtrcFcltyType}' : '구조물 : 정보 없음',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  widget.item.rdnmadr != 'null' ?
+                  '주소 : ${widget.item.rdnmadr}'
+                  :'주소 : 정보 없음',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+
+                SizedBox(height: 10),
+                Text(widget.item.phoneNumber != 'null' ?
+                  '전화번호 : ${widget.item.phoneNumber}' : '전화번호 : 정보 없음',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
