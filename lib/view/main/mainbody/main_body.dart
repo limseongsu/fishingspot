7ce9f951a925db94cd3d2a7242cbe879ae350dd2
imseongsu <tjdtn2323@naver.com>
2021-08-20 16:31:23 +0900

    Local DB 연동 main_page

--- lib/view/main/mainbody/main_body.dart
+++ lib/view/main/mainbody/main_body.dart
@@ -5,9 +5,6 @@ import 'package:get/get.dart';
 import 'mainbodyitems/carousel_slider.dart';
 
 class MainBody extends StatefulWidget {
-
-
-
   @override
   State<MainBody> createState() => _MainBodyState();
 }
@@ -15,20 +12,16 @@ class MainBody extends StatefulWidget {
 class _MainBodyState extends State<MainBody> {
   final apiViewModel = Get.find<ApiViewModel>();
 
-
-
   @override
   Widget build(BuildContext context) {
-
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          //TODO 드랍다운 메뉴 만들기
           Row(
             children: [
               Padding(
-                padding: const EdgeInsets.only(top: 10,bottom: 10),
+                padding: const EdgeInsets.only(top: 10, bottom: 10, left: 10),
                 child: Text(
                   '목록', //Todo 몇 km 인지 확인
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
@@ -39,9 +32,10 @@ class _MainBodyState extends State<MainBody> {
           Divider(color: Colors.grey[300]),
           Carousel(apiViewModel),
           Padding(
-            padding: const EdgeInsets.only(top:8.0,bottom: 8),
-            child: Text('가까운 순서',
-            style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold),
+            padding: const EdgeInsets.only(top: 8.0, bottom: 8, left: 10),
+            child: Text(
+              '가까운 순서',
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
             ),
           ),
           Divider(color: Colors.grey[300]),
